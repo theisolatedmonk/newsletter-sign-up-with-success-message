3ca43647fd5b6bec3bba450e658ddf72a7236353
theisolatedmonk <96rahulseth@gmail.com>
2023-05-31 23:45:04 +0530

    initial-commit

---
+++ src/app/List.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import iconList from '@/assets/images/icon-list.svg'
+
+type ListMsg = {
+    text: string
+}
+
+export default function List(props:ListMsg) {
+  return (
+    <div className="flex">
+    <Image src={iconList} alt={''}/>
+    <p className="text-sm">{props.text}</p>
+ 
+   
+     
+    </div>
+  )
+}

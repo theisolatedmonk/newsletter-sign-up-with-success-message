c132236701cd5c2f02380b3cadf88973f1396526
theisolatedmonk <96rahulseth@gmail.com>
2023-06-01 13:09:12 +0530

    component-adjusted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,32 @@
-import Image from 'next/image'
-import Imaillustration from '@/assets/images/illustration-sign-up-desktop.svg'
-import List from './List'
-
-
+import Image from "next/image";
+import Imaillustration from "@/assets/images/illustration-sign-up-desktop.svg";
+import List from "./List";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex text-black bg-white w-full">
-        <div className="">
-          <p className="font-bold">Stay Updated!</p>
-          <p className="text-sm">Join 60,000+ product managers receiving monthly updates on:</p>
-         <List text={'Product discovery and building what matters '}/>
-         <List text={'Measuring to ensure updates are a success '}/>
-         <List text={'And much more!'}/>
-         <div className="flex flex-col">
-          <p className="text-"></p>
-          <input type="email" placeholder='email@company.com' />
-          <p className="font-bold">Subscribe to monthly newsletter</p>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[hsl(234,9%,20%)]  w-[1440px] ">
+      <div className="flex text-black bg-white p-8 w- gap-6 items-center ">
+        <div className="   justify-center items gap-12 border-0 flex flex-col items-start">
+          <p className="font-bold text-5xl">Stay Updated!</p>
+          <p className="text-sm">
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+         <div className="flex flex-col gap-2">
+         <List text={"Product discovery and building what matters "} />
+          <List text={"Measuring to ensure updates are a success "} />
+          <List text={"And much more!"} />
          </div>
+          <div className="flex flex-col w-full gap-2">
+            <p className="text-[hsl(234,29%,20%)]">Email address</p>
+            <input type="email" placeholder="email@company.com" className="p-3 border rounded-md"/>
+            <p className="font-bold text-white text-center bg-[hsl(234,29%,20%)] p-3 rounded-md ">
+              Subscribe to monthly newsletter
+            </p>
+          </div>
         </div>
 
-
-
-        <Image src={Imaillustration} alt={''}/>
+        <Image className="w-[50%]" src={Imaillustration} alt={""} />
       </div>
     </main>
-  )
+  );
 }
